96be338c9706e2e3c4896f3a3e7da1d470b74e85
Jason Yuen <jasony@nulogy.com>
2015-12-02 23:42:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buzzbuzz::Application.config.secret_key_base = '2b2e67a17a5d8107d2e4992629b2355b2e1a61802a6790872a40674907a322748e8d79ea2527766b445e6fce1884f5e06bdb1a29839b8e4f0f412e574e2ba3d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Buzzbuzz::Application.config.session_store :cookie_store, key: '_buzzbuzz_session'
