720a0bdd4166b59b1705f3e831441de533b0fb1e
Jason Yuen <jasony@nulogy.com>
2015-12-08 00:15:39 -0500

    renames accepted to winner

--- app/controllers/buzzes_controller.rb
+++ app/controllers/buzzes_controller.rb
@@ -3,6 +3,6 @@ class BuzzesController < ApplicationController
     buzz_session = BuzzSession.last
     winning_buzz_id = buzz_session.winning_buzz_id
     buzz = Buzz.create! buzz_session_id: buzz_session.id, name: params[:name]
-    render json: { :accepted => (buzz.id == winning_buzz_id) }
+    render json: { :winner => (buzz.id == winning_buzz_id) }
   end
 end
--- spec/controllers/buzzes_controller_spec.rb
+++ spec/controllers/buzzes_controller_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe BuzzesController, type: :controller do
     BuzzSession.create! winning_buzz_id: 1
   end
 
-  it "creates an accepted buzz when it's the first one of the buzz session" do
+  it "creates an winning buzz when it's the first one of the buzz session" do
     post :create
 
     expect(response.status).to eq(200)
     data = OpenStruct.new(JSON.parse(response.body))
-    expect(data.accepted).to eq(true)
+    expect(data.winner).to eq(true)
   end
 
-  it "does not create an accepted buzz when it's the second one" do
+  it "does not create an winning buzz when it's the second one" do
     Buzz.create!
 
     post :create
 
     expect(response.status).to eq(200)
     data = OpenStruct.new(JSON.parse(response.body))
-    expect(data.accepted).to eq(false)
+    expect(data.winner).to eq(false)
   end
 
   it "buzzes are associated with a buzz session" do
