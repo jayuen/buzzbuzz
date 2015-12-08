6e52d42eadee7e9a48d144a620c4b8811a046265
Jason Yuen <jasony@nulogy.com>
2015-12-08 01:01:38 -0500

    handles case when no buzz session exists

--- app/controllers/buzzes_controller.rb
+++ app/controllers/buzzes_controller.rb
@@ -1,8 +1,13 @@
 class BuzzesController < ApplicationController
   def create
     buzz_session = BuzzSession.last
-    winning_buzz_id = buzz_session.winning_buzz_id
-    buzz = Buzz.create! buzz_session_id: buzz_session.id, name: params[:name]
-    render json: { :winner => (buzz.id == winning_buzz_id) }
+
+    if buzz_session.present?
+      winning_buzz_id = buzz_session.winning_buzz_id
+      buzz = Buzz.create! buzz_session_id: buzz_session.id, name: params[:name]
+      render json: { :winner => (buzz.id == winning_buzz_id) }
+    else
+      render json: { error: 'No buzz session. POST to /buzz_sessions first to create a new session' }, status: 422
+    end
   end
 end
--- spec/controllers/buzzes_controller_spec.rb
+++ spec/controllers/buzzes_controller_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BuzzesController, type: :controller do
-  before do
-    BuzzSession.create! winning_buzz_id: 1
+  it "requires a buzz_session" do
+    post :create
+
+    expect(response.status).to eq(422)
+    data = OpenStruct.new(JSON.parse(response.body))
+    expect(data.error).to match(/No buzz session/)
   end
 
   it "creates an winning buzz when it's the first one of the buzz session" do
+    create_buzz_session
     post :create
 
     expect(response.status).to eq(200)
@@ -14,6 +19,7 @@ RSpec.describe BuzzesController, type: :controller do
   end
 
   it "does not create an winning buzz when it's the second one" do
+    create_buzz_session
     Buzz.create!
 
     post :create
@@ -24,8 +30,14 @@ RSpec.describe BuzzesController, type: :controller do
   end
 
   it "buzzes are associated with a buzz session" do
+    create_buzz_session
+
     expect do
       post :create
     end.to change { BuzzSession.last.buzzes.size }
+  end
+
+  def create_buzz_session
+    BuzzSession.create! winning_buzz_id: 1
   end
 end
