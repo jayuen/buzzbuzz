5d39ae1782a089b42de8401868d3e372e13157d9
Jason Yuen <jasony@nulogy.com>
2015-12-07 22:38:12 -0500

    adds tests

--- test/controllers/buzzes_controller_test.rb
+++ test/controllers/buzzes_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class BuzzesControllerTest < ActionController::TestCase
-  test "should create and be accepted" do
+  before do
+    BuzzSession.create!
+  end
+
+  test "create an accepted buzz when it's the first one" do
+    post :create
+
+    assert_equal 200, response.status
+    data = OpenStruct.new(JSON.parse(response.body))
+
+    assert_equal true, data.accepted
+  end
+
+  test "create a not accepted buzz when it's not the first one" do
     post :create
 
     assert_equal 200, response.status
