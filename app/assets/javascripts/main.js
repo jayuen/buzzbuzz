395dbc005cd23e4cbd5b643c24472ac1177c9002
Jason Yuen <jasony@nulogy.com>
2015-12-11 15:11:20 -0500

    displays the name of the winning buzz

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,11 @@
 $(function() {
   // Here we instantiate a new WebSocketRails instance
   dispatcher = new WebSocketRails("localhost:3000/websocket");
-  var channel = dispatcher.subscribe('default');
+  var channel = dispatcher.subscribe('winning_buzzes');
 
   // We bind the incoming message event
   channel.bind('winner',
     function(message) {
-      document.querySelector('#winner-result').innerHTML = message;
+      document.querySelector('#winner-result').innerHTML = message.name;
     })
 });
--- app/controllers/buzzes_controller.rb
+++ app/controllers/buzzes_controller.rb
@@ -5,12 +5,18 @@ class BuzzesController < ApplicationController
     if buzz_session.present?
       winning_buzz_id = buzz_session.winning_buzz_id
       buzz = Buzz.create! buzz_session_id: buzz_session.id, name: params[:name]
+      winning_buzz = (buzz.id == winning_buzz_id)
+      notify(params[:name], winning_buzz)
 
-      WebsocketRails[:default].trigger(:winner, "winner #{buzz.id}!")
-
-      render json: { :winner => (buzz.id == winning_buzz_id) }
+      render json: { :winner =>  winning_buzz}
     else
       render json: { error: 'No buzz session. POST to /buzz_sessions first to create a new session' }, status: 422
+    end
+  end
+
+  def notify(name, winning_buzz)
+    if winning_buzz
+      WebsocketRails[:winning_buzzes].trigger(:winner, {name: name})
     end
   end
 end
