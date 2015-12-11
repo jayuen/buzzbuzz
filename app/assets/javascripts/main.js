9851405ba8ff93e1cd221d605c84242312fccf84
Jason Yuen <jasony@nulogy.com>
2015-12-11 16:01:23 -0500

    inlines channel variable

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,10 @@
 $(function() {
   // Here we instantiate a new WebSocketRails instance
   dispatcher = new WebSocketRails("localhost:3000/websocket");
-  var channel = dispatcher.subscribe('winning_buzzes');
 
-  // We bind the incoming message event
-  channel.bind('winner',
+  dispatcher.subscribe('winning_buzzes').bind('winner',
     function(message) {
       document.querySelector('#winner-result').innerHTML = message.name;
-    })
+    }
+  )
 });
